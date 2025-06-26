38844bea06c768dd8b7bce80950da4ce5364718b
lahiruasiri <lahiruasiri1@gmail.com>
2025-06-26 21:55:44 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -218,7 +218,7 @@ function ControlPanel({ position, rotation, onPositionChange, onRotationChange }
 }
 
 export function App() {
-  const [position, setPosition] = useState({ x: 0, y: 0.9, z: -0.5 })
+  const [position, setPosition] = useState({ x: 0, y: 0.5, z: 0 })
   const [rotation, setRotation] = useState({ x: 0, y: 0, z: 0 })
 
   useEffect(() => {
